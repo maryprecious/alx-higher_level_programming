34a98e6872602bafaf38d41263b3ffbd53d4dd48
maryprecious <maryprecious144@gmail.com>
2023-12-08 17:19:58 -0800

    adding all unique integers in a list just once for each integer

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -6,12 +6,11 @@ def uniq_add(my_list=[]):
     integers in a list (only once for each integer).
     """
 
-    veryunique_integers = set()
-
-     for num in my_list:
-        veryunique_integers.add(num)
-
-         sum_veryunique = sum(veryunique_integers)
-
-         return sum_veryunique
+    new_list = []
+    sum = 0
+    for num in my_list:
+        if num not in new_list:
+            sum += num
+            new_list.append(num)
+    return sum
 
