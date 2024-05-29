5ff3202791e5abc708ed643938ac5be36c29d48a
maryprecious <maryprecious144@gmail.com>
2024-05-29 13:41:58 -0700

    computes and prints

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const myArg = process.argv;
+
+function factorial(n)
+{
+	if (isNaN(n))
+	{
+		return 1;
+	}
+	else
+		return n * factorial(n - 1);
+}
+
+console.log(factorial(Number(myArg[2])));
+// console.log(Number(myArg[2]));
