648eb348a4e687fb5cf08c0c40237c57f79e1a35
maryprecious <maryprecious144@gmail.com>
2024-05-29 13:06:13 -0700

    prints x times c is fun

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const myArgs = process.argv;
+
+if (myArgs.length === 2) { console.log('Missing number of occurrences'); } else {
+  let i = 0;
+  while (i < myArgs[2]) {
+    console.log('C is fun');
+    i++;
+  }
+}
